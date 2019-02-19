633b23a0cdff680fd3424e5acee353ec2d11f7fb
caltexrui <rjia@caltex.com.au>
2019-02-19 14:42:19 +1100

    Updated message

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "The website for Penrith Chinese Presbyterian Church is under construction! For more information, please contact: Darren, 0430066645\n彭里斯华人长老会网站还在建设中！请联系牧师助理：大海 0430066645"
     }
     
     // Basic "Hello, world!" example
